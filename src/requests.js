59e2a4a6a2a14986b5edb5ca044648021113431d
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-03-04 18:06:26 +0000

    set up requests

---
+++ src/requests.js
@@ -0,0 +1,7 @@
+export const getShowsByTypeKey = key => {
+    return fetch(`https://api.tvmaze.com/search/shows?q=${key}`).then(resp => resp.json());
+}
+
+export const getShowsByTypeId = id => {
+    return fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`).then(resp => resp.json());
+}
