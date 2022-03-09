76250a654a2d0061102447f7f8b367c7bf9ae27d
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-03-09 13:41:04 +0000

    fixing bugs with shownamebuttons creation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 
 import { mapListToDOMElements } from './dominteractions.js';
+import { getShowsByTypeKey } from './requests.js';
 
 class TvMaze {
     constructor() {
@@ -12,25 +13,37 @@ class TvMaze {
     initializeApp = () => {
         this.connectDOMElements();
         this.setupListeners();
+        this.fetchAndDisplayShows();
     }
 
     connectDOMElements = () => {
         const listOfIds = Array.from(document.querySelectorAll('[id]')).map(elem => elem.id);
         const listOfShowNames = Array.from(document.querySelectorAll('[data-show-name]')).map(elem => elem.dataset.showName);
 
+        console.log(document.querySelectorAll('[id]'));
         console.log(listOfIds);
         console.log(listOfShowNames);
         
         this.viewElems = mapListToDOMElements(listOfIds, 'id');
-        this.showNameButtons = mapListToDOMElements(listOfIds, "listOfShowNames");
+        this.showNameButtons = mapListToDOMElements(listOfShowNames, 'data-show-name');
         console.log(this.viewElems);
         console.log(this.showNameButtons);
+        console.log(Object.keys(this.showNameButtons));
     }
 
     setupListeners = () => {
         Object.keys(this.showNameButtons).forEach(showName => {
-            this.showNameButtons[showName].addEventListener('click', this.setCurrentNameFilter);
+            this.showNameButtons[showName].addEventListener('click', this.setCurrentNameFilter)
         });
+    }
+
+    setCurrentNameFilter = () => {
+        this.selectedName = event.target.dataset.showName;
+        this.fetchAndDisplayShows();
+    }
+
+    fetchAndDisplayShows = () => {
+        getShowsByTypeKey(this.selectedName).then(shows => console.log(shows));
     }
 }
 
